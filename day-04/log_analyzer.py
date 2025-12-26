f156d5e17bd3de8d110896266e65ac4ff9f9fbad
gauravr <gauravr397@gmail.com>
2025-12-26 20:42:42 +0530

    log analyze

---
+++ day-04/log_analyzer.py
@@ -0,0 +1,44 @@
+import sys
+import urllib3
+
+def analyze_log(input_file_path):
+    log_count={
+        "INFO":0,
+        "WARNING":0,
+        "ERROR":0
+    }
+
+    try:
+        with open(input_file_path, 'r') as file:
+            for line in file:
+                if "INFO" in line:
+                    log_count["INFO"]+=1
+                elif "WARNING" in line:
+                    log_count["WARNING"]+=1
+                elif "ERROR" in line:
+                    log_count["ERROR"]+=1
+                
+        return log_count
+
+    except Exception as e:
+        print(f"An exception error occured: {e}")
+        return None
+
+def save_summary(count, output_file):
+    with open(output_file, 'w') as file:
+        file.write("Log file Report")
+        for key,value in count.items():
+            file.write(f"{key}:{value}\n")
+
+if __name__ == "__main__":
+    input_log_file="app.log"
+    output_summary_file="log_summary.txt"
+
+    summary_data=analyze_log(input_log_file)
+
+    if summary_data:
+        print(f"---SUmmary---")
+        for key,value in summary_data.items():
+            print(f"{key}:{value}")
+
+        save_summary(summary_data,output_summary_file)
